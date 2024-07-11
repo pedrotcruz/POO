a47f1777d172fce380a38ad69af9bab9944ad5c4
pedrotcruz <pedrotrog14.cruz@gmail.com>
2024-07-11 16:11:06 +0100

    Delete Rogue_AfonsoTaveira_99198_PedroCruz_99379 directory

--- Rogue_AfonsoTaveira_99198_PedroCruz_99379/src/pt/iscte/poo/example/MainExample.java
+++
@@ -1,9 +0,0 @@
-package pt.iscte.poo.example;
-
-import java.io.FileNotFoundException;
-
-public class MainExample {
-	public static void main(String[] args) throws FileNotFoundException {
-		GameEngine.getInstance().start();
-	}
-}
